5490dcfee93c6c24d9eb03e60b8d045a5b38809a
samskzi <71952214+samskzi@users.noreply.github.com>
2020-10-13 22:01:40 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,7 +3,6 @@ window.onload = function() {
     let board1 = document.getElementById('board');
     var s = board1.children;
     for(var i = 0; i <= 9; ++i){
-        var s1 = s[i];
         s[i].classList.add('square');
     }
     }
